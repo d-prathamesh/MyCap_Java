5e11f0b8a2578c2165ea0a4a6f5661ab5baf1e5a
d-prathamesh <69900104+d-prathamesh@users.noreply.github.com>
2020-11-13 12:07:31 +0530

    Create GradeCard.java

---
+++ GradeCard.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mycaptain;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author deoka
+ */
+public class GradeCard {
+     public static void main(String args[])
+    {
+        int marks[] = new int[6];
+        int i;
+        float total=0, avg;
+        Scanner GradeSheet = new Scanner(System.in);
+		
+        
+        for(i=0; i<6; i++) { 
+           System.out.print("Enter Marks of Subject"+(i+1)+":");
+           marks[i] = GradeSheet.nextInt();
+           total = total + marks[i];
+        }
+        GradeSheet.close();
+        //Calculating average here
+        avg = total/6;
+        System.out.print("The student Grade is: ");
+        if(avg>=80)
+        {
+            System.out.print("A");
+        }
+        else if(avg>=60 && avg<80)
+        {
+           System.out.print("B");
+        } 
+        else if(avg>=40 && avg<60)
+        {
+            System.out.print("C");
+        }
+        else
+        {
+            System.out.print("D");
+        }
+    }
+}
